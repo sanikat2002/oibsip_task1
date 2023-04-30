d82e3b44dead60eb149d8da77d4aadef533219d6
sanikat2002 <127009537+sanikat2002@users.noreply.github.com>
2023-04-30 12:56:56 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('button'));
+buttons.map( button => {
+    button.addEventListener('click',(e) => {
+        switch(e.target.innerText){
+            case 'C':
+            display.innerText = '';
+            break;
+        case '=':    //for the result
+            try{
+                display.innerText = eval(display.innerText);    //case of any other error
+            }catch {
+                display.innerText = "Error";
+            }
+            break;
+        case '+':
+             if (display.innerText){    
+              display.innnerText = display.innerText.slice(0, -1);
+            }
+            break;
+        default:
+                display.innerText +=e.target.innerText;
+        }
+    });
+});
